0da081f65df970643f3f2bb023ed55ad5ed1d52f
Josué Alonzo Chavarría <joshi.alonzo@gmail.com>
2020-11-21 17:46:45 -0600

    Social: feed and profile views

--- webserver/apps/social/urls.py
+++ webserver/apps/social/urls.py
@@ -1,4 +1,11 @@
+# built-in django modules
 from django.urls import path
 
+# custom django modules
+from .views import feed
+from .views import profile
+
 urlpatterns = [
+    path('', feed, name='feed'),
+    path('profile/', profile, name='profile'),
 ]
