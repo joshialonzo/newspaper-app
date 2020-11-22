7f02ce0fe5ce01433d367d5f4184c3146fd391cb
Josué Alonzo Chavarría <joshi.alonzo@gmail.com>
2020-11-21 21:38:07 -0600

    Create profile and user together

--- webserver/apps/social/models.py
+++ webserver/apps/social/models.py
@@ -1,6 +1,8 @@
 # built-in django modules
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -8,6 +10,13 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(default='batman.png')
+    objects = models.Manager()
 
     def __str__(self):
         return f'Perfil de {self.user.username}'
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
