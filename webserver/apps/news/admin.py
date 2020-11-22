2e3e2ded233c145855c644714bdeac9b82817518
Josué Alonzo Chavarría <joshi.alonzo@gmail.com>
2020-11-21 18:31:25 -0600

    Register profile and new in admin

--- webserver/apps/news/admin.py
+++ webserver/apps/news/admin.py
@@ -1,3 +1,10 @@
+# built-in django modules
 from django.contrib import admin
 
+# custom django modules
+from .models import New
+
 # Register your models here.
+
+
+admin.site.register(New)
--- webserver/apps/social/admin.py
+++ webserver/apps/social/admin.py
@@ -1,3 +1,10 @@
+# built-in django modules
 from django.contrib import admin
 
+# custom django modules
+from .models import Profile
+
 # Register your models here.
+
+
+admin.site.register(Profile)
