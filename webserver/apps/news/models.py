f9b7a3d7472a647c66ac82877f6e4a5fd52d8333
Josué Alonzo Chavarría <joshi.alonzo@gmail.com>
2020-11-21 18:19:43 -0600

    New model: new

--- webserver/apps/news/models.py
+++ webserver/apps/news/models.py
@@ -1,3 +1,18 @@
+# built-in django modules
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class New(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news')
+    timestamp = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content}'
