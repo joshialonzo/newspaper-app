b8960a03cbeb7bc855a3b0c6fa5b88b4e2d82f8a
Josué Alonzo Chavarría <joshi.alonzo@gmail.com>
2020-11-22 20:38:47 -0600

    Add new fields to new model

--- webserver/apps/news/models.py
+++ webserver/apps/news/models.py
@@ -10,14 +10,22 @@ from django.utils import timezone
 
 
 class New(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(unique=True, null=True)
+    status = models.CharField(choices=STATUS_CHOICES, default='draft', max_length=10)
+    publication = models.DateTimeField(default=timezone.now)
     content = models.TextField()
 
     class Meta:
-        ordering = []
+        ordering = ['-publication']
 
     def __str__(self):
         return f'{self.author.username}: {self.content}'
