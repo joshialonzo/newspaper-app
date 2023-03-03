b274d4b98839e0f7f602cea8759b94da81225f71
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-03-02 18:25:17 -0600

    Add carousel to home

--- webserver/apps/news/views.py
+++ webserver/apps/news/views.py
@@ -134,4 +134,10 @@ def yucatan_news(request):
 
 
 def bootstrap_home(request):
-    return render(request, 'bootstrap/home.html', {})
+    # text news
+    last_5_news = None
+    text_news = get_text_news(None)
+    if text_news.exists():
+        last_5_news = text_news[:5]
+    context = {'last_5_news': last_5_news}
+    return render(request, 'bootstrap/home.html', context)
